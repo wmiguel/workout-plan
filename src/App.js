05f85f1490c5db387da7f17c61bb7f543856f43c
William Miguel <wmiguel4815@gmail.com>
2024-10-16 13:24:34 -0700

    Finished and New Logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import workouts from "./api/workouts";
+import dayjs from "dayjs";
+
+const style = {
+  bg: `w-screen h-screen px-4 py-8 bg-[#EAE7E7]`,
+  details: `bg-white w-full rounded overflow-hidden`,
+  summary: `flex font-semibold text-xl justify-between items-center bg-[#ff403d] p-4`,
+  data: `flex flex-col gap-4 pt-4`,
+  span: `text-red-500 ml-2 font-medium text-xs uppercase`,
+  footer: `text-center pt-4`,
+};
 
 function App() {
+  const today = dayjs().format("dddd");
+  const todaysWorkout = workouts.filter((date) => date.day === today);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="bg-[#ff403d] p-4">
+        <div className="mx-auto max-w-screen-lg">
+          <div className="flex items-center justify-between bg-[#160203] p-1.5 w-fit gap-2">
+            <span className="text-xl">💪</span>
+            <p className="text-xl font-medium text-[#fefbea]">
+              Workout of the Day
+            </p>
+          </div>
+        </div>
       </header>
-    </div>
+
+      <section className={style.bg}>
+        <div className="flex flex-wrap gap-4 justify-between mx-auto max-w-screen-lg">
+          {todaysWorkout.length === 0 ? <p>Relax! Take a break today.</p> : null}
+          {todaysWorkout.map((today, index) => (
+            <div key={index} className={style.details}>
+              <div className={style.summary}>
+                <span>
+                  {today.day} - {today.title}
+                </span>
+              </div>
+
+              <div className={style.data}>
+                <table>
+                  <tbody>
+                    {today.workout.map((workout, index) => (
+                      <tr key={index}>
+                        <td className="font-medium p-2">{workout.exercise}</td>
+                        <td className="font-light p-2">{workout.reps}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ))}
+        </div>
+        <p className={style.footer}>Coded by William Miguel</p>
+      </section>
+    </>
   );
 }
 
---
+++ src/api/workouts.js
@@ -0,0 +1,126 @@
+const workouts = [
+  {
+    day: "Monday",
+    title: "Chest, Triceps, and Shoulders",
+    workout: [
+      {
+        exercise: "Incline Bench Press (D/B)",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Flat Bench Press (D/B)",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Shoulder Press / Arnold Press",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Lateral Raise",
+        reps: "3x10-15",
+      },
+      {
+        exercise: "Tricep Extension",
+        reps: "3x10-15",
+      },
+    ],
+  },
+  {
+    day: "Tuesday",
+    title: "Back, Traps, and Biceps",
+    workout: [
+      {
+        exercise: "Bent Over Row",
+        reps: "3x8-10",
+      },
+      {
+        exercise: "Lat Pull Down (Banded)",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Isolated Rows (Banded)",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Shrugs",
+        reps: "3x10-15",
+      },
+      {
+        exercise: "Bicep / Pause Curl",
+        reps: "3x10-15",
+      },
+    ],
+  },
+  {
+    day: "Wednesday",
+    title: "Legs and Abs",
+    workout: [
+      {
+        exercise: "Squats",
+        reps: "3x8-10",
+      },
+      {
+        exercise: "Romanian Deadlifts",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Leg Extensions",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Calf Raises",
+        reps: "3x10-15",
+      },
+      {
+        exercise: "Hanging Leg Raises",
+        reps: "3x5-8",
+      },
+    ],
+  },
+  {
+    day: "Thursday",
+    title: "Upper",
+    workout: [
+      {
+        exercise: "Bench Press",
+        reps: "3x8-10",
+      },
+      {
+        exercise: "Bent Over Row",
+        reps: "3x8-10",
+      },
+      {
+        exercise: "Pull Ups",
+        reps: "3 x failure",
+      },
+      {
+        exercise: "Bicep / Pause Curl",
+        reps: "3x10-15",
+      },
+      {
+        exercise: "Tricep Extension",
+        reps: "3x10-15",
+      },
+    ],
+  },
+  {
+    day: "Friday",
+    title: "Lower",
+    workout: [
+      {
+        exercise: "Bulgarian Split Squat",
+        reps: "3x10-12",
+      },
+      {
+        exercise: "Leg Press",
+        reps: "3x10-15",
+      },
+      {
+        exercise: "Hamstring Curl",
+        reps: "3x10-12",
+      },
+    ],
+  },
+];
+
+export default workouts;
